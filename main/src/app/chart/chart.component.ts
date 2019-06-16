15602dec38212f7868c67156c54f88c37bc996f8
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-06-16 14:46:07 +0900

    refactoring chart datas

--- main/src/app/chart/chart.component.ts
+++ main/src/app/chart/chart.component.ts
@@ -11,103 +11,47 @@ export class ChartComponent implements OnInit,AfterViewInit,OnChanges {
   @ViewChild('canvas',{static: false})
   ref: ElementRef;
 
-  // @Input()
-  // data: ChartData;
+  @Input() width: number;
+  @Input() height: number;
 
-  // @Input()
-  // options: ChartOptions;
-
-  // @Input() labels: string[];
-  // @Input() datas: object[];
-  // @Input() yAxes:object[];
-  isBeginzeros:boolean = false;
-
-  context: CanvasRenderingContext2D;
-  chart: Chart;
-  selectIndex:number = 0;
-  colorList = ['rgb(223, 215, 0)','rgb(255, 163, 0)','rgb(255, 163, 255 )'];
+  private context: CanvasRenderingContext2D;
+  private chart: Chart;
+  private color = [ 'rgba(255,99,132,1)' ];
+  private label = "label";
+  private data = [12, 19, 3, 5, 2, 3];
+  private labels = ["12", "19", "3", "5", "2", "3"];
 
   constructor() { }
 
   ngOnInit() {
-    this.initDate();
   }
   ngOnChanges(){
-    // this.data.labels = this.labels;
-    
     this.chart.update();
   }
   
-  private initDate(){
-    // this.data = {
-    //   labels: this.labels,
-    //   datasets: [{
-    //     label: 'humidity',
-    //     data: [],
-    //     borderColor: this.colorList[0],
-    //     lineTension: 0, //<===追加
-    //     fill: false,    //<===追加
-    //   },
-    //   {
-    //     label: 'temperature',
-    //     data: [],
-    //     borderColor: this.colorList[1],
-    //     lineTension: 0, //<===追加
-    //     fill: false,    //<===追加
-    //   }]
-    // };
-
-    // this.options = {
-    //   scales: {
-    //     yAxes: this.yAxes
-    //   },
-    //   responsive: true
-    // };
-  }
- 
   ngAfterViewInit() {
     // canvasを取得
     this.context = this.ref.nativeElement.getContext('2d');
-    this.context.canvas.height = window.innerHeight / 5;
-    let data = {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    this.context.canvas.height = window.innerHeight / 6;
+    let data:Chart.ChartData = {
+      labels: this.labels,
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: this.label,
+        data: this.data,
         backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 255, 255, 0.2)',
         ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
+        borderColor: this.color,
+        borderWidth: 1,
+        
       }]
     };
-    // チャートの作成
-    Chart.defaults.global.defaultFontColor = 'white';
+    
+    Chart.defaults.global.defaultFontColor = 'black';
     this.chart = new Chart(this.context, {
       type: 'line',
-      data: data,      // データをプロパティとして渡す
-      options: this.options, // オプションをプロパティとして渡す
+      data: data,
     });
-  }
-
-  setDateOnGraph(index:number){
-    this.selectIndex = index;
-    this.chart.update();
-  }
-  isActive(index:number){
-    return this.selectIndex == index;
   }
 
 }
