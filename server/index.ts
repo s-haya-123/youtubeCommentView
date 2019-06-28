02d7c79aa8112f6e0d322426908e11f4c86634e9
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-06-28 14:23:09 +0900

    add statics functions

--- server/index.ts
+++ server/index.ts
@@ -7,10 +7,26 @@ export const hello = (req: any,res:any) => {
 export const getComment = async (req:any,res:any) => {
     const db = new CommentDatabaseLocalPostgres()
     const movieId = req.query.movie_id;
+    const bin = "bin" in req.query ? Number(req.query.bin) : 30000;
     const commets = await getComments(db,movieId);
-    res.status(200).send(commets);
+    const statics = takeStaticsOfComment(commets,bin);
+    res.status(200).send(statics);
 }
 
 async function getComments(db: CommentDatabase ,movieId: string): Promise<YoutubeCommentRow[]> {
     return db.getComments(movieId);
+}
+
+function　takeStaticsOfComment(comments: YoutubeCommentRow[], bin: number) {
+    const binRange = calcBinRange(comments,bin);
+    const results = new Array(binRange).fill(0);
+    return comments.reduce((acc: number[],comment: YoutubeCommentRow) => {
+        const index = Math.floor(Number(comment.timestampMsec) / bin);
+        acc[index] += 1;
+        return acc;
+    }, results);
+}
+function calcBinRange(comments: YoutubeCommentRow[], binMsec: number): number {
+    const maxTimestamp = Number(comments.slice(-1)[0].timestampMsec);
+    return Math.floor(maxTimestamp / binMsec) + 1;
 }
