3f3a653e4863b7d158cdcdb5b6ed295dd27986f7
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-06-30 15:05:12 +0900

    rendering thumbnail array

--- main/src/app/list/list.component.ts
+++ main/src/app/list/list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+class YoutubeThumbnail {
+  constructor(
+    public title: string,
+    public url: string
+  ){}
+}
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
+  private thumbnails: YoutubeThumbnail[] = [new YoutubeThumbnail("title", "./assets/icon.png"),new YoutubeThumbnail("title2", "./assets/icon.png")];
   constructor() { }
 
   ngOnInit() {
