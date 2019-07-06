a49ffaf85e06b71c940219906d4282ff4d872b6b
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-07-06 16:14:32 +0900

    change chart shape and add click event on bar

--- main/src/app/chart/chart.component.ts
+++ main/src/app/chart/chart.component.ts
@@ -32,6 +32,8 @@ export class ChartComponent implements AfterViewInit,OnChanges {
   private chartDatas:CommentData[];
   messages: string[] = [];
   private messageRange = 60000;
+  private targetColor = 'rgba(204,0,51, 0.2)';
+  private otherColor = ' rgba(0,0,0, 0.1)';
 
 
   constructor(private youtubeService: YoutubeService) { 
@@ -54,23 +56,22 @@ export class ChartComponent implements AfterViewInit,OnChanges {
     this.context = this.ref.nativeElement.getContext('2d');
     let labels = this.chartDatas.map( (chartData: CommentData) => chartData.label );
     let datas = this.chartDatas.map( (chartData: CommentData)=> chartData.commentNumber);
+    let backgroudColor = new Array(datas.length).fill(this.otherColor);
 
     let data:Chart.ChartData = {
       labels: labels,
       datasets: [{
         label: this.label,
         data: datas,
-        backgroundColor: [
-            'rgba(255, 255, 255, 0.2)',
-        ],
-        borderColor: this.color,
+        backgroundColor: backgroudColor,
+        hoverBackgroundColor: backgroudColor,
         borderWidth: 1,
       }]
     };
     
     Chart.defaults.global.defaultFontColor = 'black';
     this.chart = new Chart(this.context, {
-      type: 'line',
+      type: 'bar',
       data: data,
       options: {
         tooltips: {
@@ -81,11 +82,26 @@ export class ChartComponent implements AfterViewInit,OnChanges {
             let index = element[0]._index;
             let chart = this.chartDatas[index];
             this.messages = this.chartDatas[index].messages;
+            this.selectChartData(index);
             this.chartClickEvent.emit(chart.second);
           }
         }
       }
     });
+  }
+  
+  selectChartData(clickIndex:number) {
+    let backgroudColors = this.chart.data.datasets[0].backgroundColor;
+    if (isStrings(backgroudColors)) {
+      this.chart.data.datasets[0].backgroundColor = backgroudColors.map((_, index)=>{
+        return index === clickIndex ? this.targetColor : this.otherColor;
+      });
+      this.chart.update();
+    }
+    
+    function isStrings(arr: string[] | any): arr is string[] {
+      return Array.isArray(arr) && typeof arr[0] === 'string';
+    }
   }
 }
 
